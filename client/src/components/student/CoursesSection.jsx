498dc955157b0b1ec7a54f365bb9e509a5d05efd
Sohan Fernando <sohanfernando7@gmail.com>
2025-03-13 22:21:12 +0530

    new commit on CourseSection.jsx

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CoursesSection = () => {
   return (
     <div>
-        <h1>Courses Section</h1>
+      <h2>Learn from the best</h2>
+      <p>Discover or top-rated courses across various categories. From coding and design to business and wellness, our courses are crafted to deliver results.</p>
+
+      <Link to={'/'}>Show all courses</Link>
     </div>
   )
 }
