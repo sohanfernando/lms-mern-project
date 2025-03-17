e68da6b936fe111a13bec69b6586a7acd8f247b5
Sohan Fernando <sohanfernando7@gmail.com>
2025-03-18 02:17:49 +0530

    TestimonialsSection done

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -6,7 +6,7 @@ import { AppContext } from '../../context/AppContext'
 
 const Navbar = () => {
 
-  const {navigate} = useContext(AppContext)
+  const {navigate, isEducator} = useContext(AppContext)
 
   const isCourseListPage = location.pathname.includes('/course-list');
 
@@ -22,7 +22,7 @@ const Navbar = () => {
           <div className='flex items-center gap-5'>
             { user && 
             <>
-              <button>Become Educator</button>
+              <button onClick={() => {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
             | <Link to='/my-enrollments'>My Enrollments</Link>
             </>
             }
@@ -36,7 +36,7 @@ const Navbar = () => {
           <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
           { user && 
             <>
-              <button>Become Educator</button>
+              <button onClick={() => {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
             | <Link to='/my-enrollments'>My Enrollments</Link>
             </>
           }
