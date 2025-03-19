302a87f502748817ecfeef54c810a5073a367eec
sohanfernando <sohanfernando7@gmail.com>
2025-03-19 11:13:33 +0530

    CourseDetails page is done

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -18,7 +18,14 @@ const Footer = () => {
             <li><a href="#">Privacy policy</a></li>
           </ul>
         </div>
-        <div></div>
+        <div className='hidden md:flex flex-col items-start w-full'>
+          <h2 className='font-semibold text-white mb-5'>Subscribe to our newsletter</h2>
+          <p className='text-sm text-white/80'>The latest news, articles, and resources, sent to your inbox weekly.</p>
+          <div className='flex items-center gap-2 pt-4'>
+            <input type="email" placeholder='Enter your email' className='border border-gray-500/30 bg-gray-800 text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm' />
+            <button className='bg-blue-600 w-24 h-9 text-white rounded'>Subscribe</button>
+          </div>
+        </div>
       </div>
       <p className='py-4 text-center text-xs md:text-sm text-white/60'>Copyright 2025 @ Edemy. All Right Reserved.</p>
     </footer>
