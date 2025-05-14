e6b3c36a82e0b50268a92be7b437184395f11a87
sohanfernando <sohanfernando7@gmail.com>
2025-05-14 14:55:29 +0530

    userRoutes updated

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js'
+import { addUserRating, getUserCourseProgress, getUserData, 
+purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js'
 
 const userRouter = express.Router()
 
