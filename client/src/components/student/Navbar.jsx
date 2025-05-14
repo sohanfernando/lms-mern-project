a4a5ae8630c7b201845d15aa077211c46a46873b
sohanfernando <sohanfernando7@gmail.com>
2025-05-14 18:16:03 +0530

    frontend updated

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -3,16 +3,38 @@ import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 import { AppContext } from '../../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Navbar = () => {
 
-  const {navigate, isEducator} = useContext(AppContext)
+  const {navigate, isEducator, backendUrl, setIsEducator, getToken} = useContext(AppContext)
 
   const isCourseListPage = location.pathname.includes('/course-list');
 
   const {openSignIn} = useClerk()
   const {user} = useUser()
   
+  const becomeEducator = async () => {
+    try {
+      if(isEducator){
+        navigate('/educator')
+        return;
+      }
+      const token = await getToken()
+      const { data } = await axios.get(backendUrl  + '/api/educator/update-role', {headers: {Authorization: `Bearer ${token}`}})
+
+      if(data.success){
+        setIsEducator(true)
+        toast.success(data.message)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
 
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14
@@ -22,7 +44,7 @@ const Navbar = () => {
           <div className='flex items-center gap-5'>
             { user && 
             <>
-              <button onClick={() => {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+              <button onClick={becomeEducator}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
             | <Link to='/my-enrollments'>My Enrollments</Link>
             </>
             }
@@ -36,7 +58,7 @@ const Navbar = () => {
           <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
           { user && 
             <>
-              <button onClick={() => {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+              <button onClick={becomeEducator}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
             | <Link to='/my-enrollments'>My Enrollments</Link>
             </>
           }
--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -6,6 +6,8 @@ import Loading from '../../components/student/Loading'
 import Footer from '../../components/student/Footer'
 import humanizeDuration from 'humanize-duration'
 import YouTube from 'react-youtube'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const CourseDetails = () => {
 
@@ -16,17 +18,58 @@ const CourseDetails = () => {
   const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false)
   const [playerData, setPlayerData] = useState(null)
 
-  const {allCourses, calculateRating, calculateNoOfLectures, 
-    calculateCourseDuration, calculateChapterTime, currency} = useContext(AppContext)
+  const {calculateRating, calculateNoOfLectures, 
+    calculateCourseDuration, calculateChapterTime, currency, backendUrl, userData, getToken} = useContext(AppContext)
 
   const fetchCourseData = async ()=>{
-    const findCourse = allCourses.find(course => course._id === id)
-    setCourseData(findCourse);
+    try {
+      const {data} = await axios.get(backendUrl + '/api/course/' + id)
+
+      if(data.success){
+        setCourseData(data.courseData)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const enrollCourse = async () => {
+    try {
+      if(!userData){
+        return toast.warn('Login to Enroll')
+      }
+      if(isAlreadyEnrolled){
+        return toast.warn('Already Enrolled')
+      }
+
+      const token = await getToken();
+
+      const { data } = await axios.post(backendUrl + '/api/user/purchase', {courseId: courseData._id}, {headers: { Authorization: `Bearer ${token}` }})
+
+      if(data.success) {
+        const {session_url} = data
+        window.location.replace(session_url)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   useEffect(() => {
     fetchCourseData()
-  },[allCourses])
+  },[])
+
+  useEffect(() => {
+    if(userData && courseData){
+      setIsAlreadyEnrolled(userData.enrolledCourses.includes(courseData._id))
+    }
+  },[userData, courseData])
 
   const toggleSection = (index) => {
     setOpenSections((prev) => (
@@ -67,7 +110,7 @@ const CourseDetails = () => {
       </div>
 
       <p className='text-sm'>Course by <span className='text-blue-600 
-      underline'>GreatStack</span></p>
+      underline'>{courseData.educator.name}</span></p>
 
       <div className='pt-8 text-gray-800'>
           <h2 className='text-xl font-semibold'>Course Structure</h2>
@@ -169,7 +212,7 @@ const CourseDetails = () => {
 
             </div>
 
-            <button className='md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 
+            <button onClick={enrollCourse} className='md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 
             text-white font-medium'
             >{isAlreadyEnrolled ? 'Already Enrolled' : 'Enroll Now'}</button>
 
